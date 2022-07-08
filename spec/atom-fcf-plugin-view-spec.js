57eeb2af8af671f2c039080940a2cb2e15eca560
Vladimir <v.o.markin221@gmail.com>
2022-07-08 05:08:44 +0300

    Initialize v.0.0.1

---
+++ spec/atom-fcf-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomFcfPluginView from '../lib/atom-fcf-plugin-view';
+
+describe('AtomFcfPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
